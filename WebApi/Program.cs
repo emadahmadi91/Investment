f7e2a2c2a76bf41ff9035f7793080b1bd6374c98
emad <emadahmadi1991@gmail.com>
2023-02-09 08:17:37 +1000

    Add Swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,12 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
 builder.Services.AddWebApiServices();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.Run();
 public partial class Program { }
